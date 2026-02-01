3e5755befa3cc9d83f5e713f369fee87f62b96a4
Purushottam <84143197+purushottam-mca@users.noreply.github.com>
2026-02-01 23:30:41 +0530

    Replace standard string functions with custom implementations

--- string.c
+++ string.c
@@ -1,19 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-/* ===============================
+/* ======================================
+   Low-level String Functions (_str*)
+   ====================================== */
+
+size_t _strlen(const char *s) {
+    size_t len = 0;
+    while (s[len] != '\0')
+        len++;
+    return len;
+}
+
+char *_strcpy(char *dest, const char *src) {
+    char *start = dest;
+    while (*src) {
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+    return start;
+}
+
+char *_strncpy(char *dest, const char *src, size_t n) {
+    size_t i = 0;
+    for (; i < n && src[i]; i++)
+        dest[i] = src[i];
+    for (; i < n; i++)
+        dest[i] = '\0';
+    return dest;
+}
+
+int _strcmp(const char *a, const char *b) {
+    while (*a && (*a == *b)) {
+        a++;
+        b++;
+    }
+    return (unsigned char)*a - (unsigned char)*b;
+}
+
+char *_strcat(char *dest, const char *src) {
+    char *start = dest;
+    while (*dest)
+        dest++;
+    while (*src)
+        *dest++ = *src++;
+    *dest = '\0';
+    return start;
+}
+
+/* ======================================
    String Data Type
-   =============================== */
+   ====================================== */
+
 typedef struct {
     char *data;
     size_t length;
     size_t capacity;
 } String;
 
-/* ===============================
-   Core Functions
-   =============================== */
+/* ======================================
+   Internal Helper
+   ====================================== */
 
 static void string_reserve(String *s, size_t new_capacity) {
     if (new_capacity <= s->capacity)
@@ -29,6 +76,10 @@ static void string_reserve(String *s, size_t new_capacity) {
     s->capacity = new_capacity;
 }
 
+/* ======================================
+   Core API
+   ====================================== */
+
 void string_init(String *s) {
     s->capacity = 16;
     s->length = 0;
@@ -37,10 +88,10 @@ void string_init(String *s) {
 }
 
 void string_from_cstr(String *s, const char *cstr) {
-    s->length = strlen(cstr);
+    s->length = _strlen(cstr);
     s->capacity = s->length + 1;
     s->data = (char *)malloc(s->capacity);
-    strcpy(s->data, cstr);
+    _strcpy(s->data, cstr);
 }
 
 void string_free(String *s) {
@@ -59,26 +110,24 @@ void string_clear(String *s) {
     s->data[0] = '\0';
 }
 
-/* ===============================
+/* ======================================
    Append Operations
-   =============================== */
+   ====================================== */
 
 void string_append_cstr(String *s, const char *cstr) {
-    size_t add_len = strlen(cstr);
+    size_t add_len = _strlen(cstr);
     size_t required = s->length + add_len + 1;
 
-    if (required > s->capacity) {
+    if (required > s->capacity)
         string_reserve(s, required * 2);
-    }
 
-    strcpy(s->data + s->length, cstr);
+    _strcat(s->data, cstr);
     s->length += add_len;
 }
 
 void string_append_char(String *s, char c) {
-    if (s->length + 2 > s->capacity) {
+    if (s->length + 2 > s->capacity)
         string_reserve(s, s->capacity * 2);
-    }
 
     s->data[s->length++] = c;
     s->data[s->length] = '\0';
@@ -88,23 +137,23 @@ void string_append(String *dest, const String *src) {
     string_append_cstr(dest, src->data);
 }
 
-/* ===============================
+/* ======================================
    Copy & Compare
-   =============================== */
+   ====================================== */
 
 void string_copy(String *dest, const String *src) {
     string_reserve(dest, src->length + 1);
-    strcpy(dest->data, src->data);
+    _strcpy(dest->data, src->data);
     dest->length = src->length;
 }
 
 int string_compare(const String *a, const String *b) {
-    return strcmp(a->data, b->data);
+    return _strcmp(a->data, b->data);
 }
 
-/* ===============================
+/* ======================================
    Substring
-   =============================== */
+   ====================================== */
 
 String string_substring(const String *s, size_t start, size_t len) {
     String sub;
@@ -117,16 +166,16 @@ String string_substring(const String *s, size_t start, size_t len) {
         len = s->length - start;
 
     string_reserve(&sub, len + 1);
-    strncpy(sub.data, s->data + start, len);
+    _strncpy(sub.data, s->data + start, len);
     sub.data[len] = '\0';
     sub.length = len;
 
     return sub;
 }
 
-/* ===============================
+/* ======================================
    Debug / Print
-   =============================== */
+   ====================================== */
 
 void string_print(const String *s) {
     printf("%s\n", s->data);
@@ -137,9 +186,9 @@ void string_debug(const String *s) {
            s->data, s->length, s->capacity);
 }
 
-/* ===============================
-   Demo (main)
-   =============================== */
+/* ======================================
+   Demo
+   ====================================== */
 
 int main() {
     String s1, s2, sub;
@@ -151,11 +200,11 @@ int main() {
     string_append(&s1, &s2);
     string_append_char(&s1, '!');
 
-    string_print(&s1);      // Hello World!
+    string_print(&s1);       // Hello World!
     string_debug(&s1);
 
     sub = string_substring(&s1, 6, 5);
-    string_print(&sub);     // World
+    string_print(&sub);      // World
 
     printf("Compare: %d\n", string_compare(&s1, &s2));
 
